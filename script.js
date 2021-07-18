165b94411312047c3e38369edf3bb2933b96108c
Shushanto Sarkar <shushantosarkar@gmail.com>
2021-07-18 23:29:07 +0530

    Car has been added(non-working car)

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-const score = document.querySelector('.score');
-const message = document.querySelector('.message');
-const road = document.querySelector('.road');
+const score = document.querySelector(".score");
+const message = document.querySelector(".message");
+const road = document.querySelector(".road");
 
-document.addEventListener('keydown', keyDown);
+console.log(road);
+
+message.addEventListener("click", gameStart);
+
+document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
 
-// console.log(road);
+let player = {};
 
-let keys = { ArrowUp : false, ArrowDown: false, ArrowLeft: false, ArrowRight: false};
+let keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+};
 
 console.log(keys);
 
-function keyDown(e){
-    e.preventDefault();
-    keys[e.key]=true;
-    console.log(e.key);
-    console.log(keys);
-}
-
-
-function keyUp(e) {
+function keyDown(e) {
   e.preventDefault();
-  keys[e.key]=false;
-  console.log(e.key);
+  keys[e.key] = true;
   console.log(keys);
 }
 
+function keyUp(e) {
+  e.preventDefault();
+  keys[e.key] = false;
+  console.log(keys);
+}
+
+function gamePlay() {
+  console.log("hey i am clicked");
+  if (player.start) {
+    window.requestAnimationFrame(gamePlay);
+  }
+}
+
+function gameStart() {
+  road.classList.remove("hide");
+  message.classList.add("hide");
+  player.start = true;
+  window.requestAnimationFrame(gamePlay);
+
+  let car = document.createElement('div');
+  car.setAttribute('class', 'car');
+  road.appendChild(car);
+}
