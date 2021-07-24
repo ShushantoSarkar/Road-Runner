d32eaebfe18d964dafe70fc22db4ac85d841698a
Shushanto Sarkar <shushantosarkar@gmail.com>
2021-07-24 13:59:41 +0530

    Car image , Name and road css

---
+++ README.md
@@ -0,0 +1,26 @@
+###Game-A-Thon
+##Road Runner
+---
+######How to Play
+
+* Press on Start Message to start the game.
+
+* Use Arrow keys to move your car.
+    * Up arrow key to move Up.
+    * Down arrow key to move Down.
+    * Left arrow key to move Left.
+    * Right arrow key to move Right.
+
+* Current score is displayed at top left corner and Previous score below that.
+
+* You can add your name at top right corner ans use change name button to change the current name.
+
+######Rules
+
+* The Speed of car will increase with respect to time and the interval is of 5sec.
+
+* The game will over when use will hit another car on road.
+
+* User can restart the game by clicking the pop-up with message GAME OVER.
+
+
--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+
+// Variable has been assing to different classes from HTML
+
 const score = document.querySelector(".score");
 const message = document.querySelector(".message");
 const road = document.querySelector(".road");
 const previousScore = document.querySelector(".previousScore");
 
+// On clicking the text game will start
 message.addEventListener("click", gameStart);
 
+// Which Key has been pressed
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
 
+// Object to initialise the speed of game and score
 let player = {
   speed: 5,
   score: 0,
 };
 
+// Initially all the key status are assigned as false
 let keys = {
   ArrowUp: false,
   ArrowDown: false,
@@ -20,16 +27,19 @@ let keys = {
   ArrowRight: false,
 };
 
+// When a key is pressed it makes the status of that key as true
 function keyDown(e) {
   e.preventDefault();
   keys[e.key] = true;
 }
 
+// When the key is released it makes the statusof the key as false
 function keyUp(e) {
   e.preventDefault();
   keys[e.key] = false;
 }
 
+// This function gives the instruction when your car overlap other car.
 function collide(a, b) {
   aRect = a.getBoundingClientRect();
   bRect = b.getBoundingClientRect();
@@ -42,6 +52,7 @@ function collide(a, b) {
   );
 }
 
+// Animation for moving the lines at the center of the road
 function moveLines() {
   let lines = document.querySelectorAll(".lines");
 
@@ -55,6 +66,7 @@ function moveLines() {
   });
 }
 
+// Animation to move and apper the normal car's on the road 
 function moveEnemyCar(car) {
   let enemy = document.querySelectorAll(".enemy");
 
@@ -74,6 +86,7 @@ function moveEnemyCar(car) {
   });
 }
 
+// Instruction for the User (Main) car to move using arrow keys
 function gamePlay() {
   let car = document.querySelector(".car");
   let position = road.getBoundingClientRect();
@@ -84,55 +97,67 @@ function gamePlay() {
     if (keys.ArrowUp && player.y > position.top + 70) {
       player.y -= player.speed;
     }
-    if (keys.ArrowDown && player.y < position.bottom - 70) {
+    if (keys.ArrowDown && player.y < position.bottom - 85) {
       player.y += player.speed;
     }
     if (keys.ArrowLeft && player.x > 0) {
       player.x -= player.speed;
     }
-    if (keys.ArrowRight && player.x < position.width - 50) {
+    if (keys.ArrowRight && player.x < position.width - 60) {
       player.x += player.speed;
     }
 
     car.style.top = player.y + "px";
     car.style.left = player.x + "px";
 
+    // Repeate the animation
     window.requestAnimationFrame(gamePlay);
 
+    // To display the score
     finalScore = player.score++;
+    
+    // To store the display the previous score
     localStorage.setItem("previousScore", finalScore);
-
-    // console.log(pScore + "pscore");
 
     score.innerText = "Current Score : " + finalScore;
   }
 }
 
+let pScore = localStorage.getItem("previousScore");
+previousScore.innerText = "Previous Score : " + pScore;
+
+// Speed of the game increase in every 5sec
 function speedIncrease() {
   currentSpeed = setInterval(function () {
     console.log("Current Speed : " + player.speed);
     player.speed++;
-  }, 3000);
-  // currentSpeed = setTimeout(function(){ console.log("Current Speed : " + player.speed);
-  //   player.speed++;
-  // }, 3000 )
+  }, 5000);
 }
 
-function enemyCarImage() {
-  let arr = [
-    "images/car1.png",
-    "images/car2.png",
-    "images/car3.png",
-    "images/car4.png",
-    "images/car5.png",
-    "images/car6.png",
-  ];
-  let randomCar = Math.floor(Math.random() * arr.length);
-  let rc = arr[randomCar];
-  return ("'(url(" + rc + "))'");
-  // return console.log (rc);
-}
+function randomColor() {
+  function c(){
+    let hex = Math.floor(Math.random()* 256).toString(16);
+    return ("0" + String(hex)).substr(-2);
+  }
+  return "#" + c() + c() + c();
+} 
 
+// function enemyCarImage() {
+//   let arr = [
+//     "images/car1.png",
+//     "images/car2.png",
+//     "images/car3.png",
+//     "images/car4.png",
+//     "images/car5.png",
+//     "images/car6.png",
+//   ];
+//   let randomCar = Math.floor(Math.random() * arr.length);
+//   let rc = arr[randomCar];
+//   return "'(url(" + rc + "))'";
+//   // return console.log (rc);
+// }
+
+// Display and Change name 
 function cName() {
   var nameAlert = prompt("Enter Your Name");
   localStorage.setItem("name", nameAlert);
@@ -140,6 +165,14 @@ function cName() {
   document.querySelector(".name").innerHTML = storedName;
 }
 
+// Displays the name stored , when page reloads
+function showName() {
+  document.querySelector(".name").innerHTML = localStorage.getItem("name");
+}
+
+showName();
+
+// Changes the message form start to game over when car hit's
 function endGame() {
   player.start = false;
   message.classList.remove("hide");
@@ -149,6 +182,7 @@ function endGame() {
     " Press here to restart the Game.";
 }
 
+// Make sure is the player DOMRectReadOnly and start's all the animations and hides the start message . 
 function gameStart() {
   message.classList.add("hide");
   road.innerHTML = "";
@@ -177,12 +211,14 @@ function gameStart() {
   player.x = car.offsetLeft;
   player.y = car.offsetTop;
 
+  // Giving positions to the enemy car and the animation
   for (let i = 0; i < 3; i++) {
     let enemyCar = document.createElement("div");
     enemyCar.setAttribute("class", "enemy");
     enemyCar.y = (i + 1) * 350 * -1;
     enemyCar.style.top = enemyCar.y + "px";
-    enemyCar.style.background = "url(images/car1.png";
+    enemyCar.style.backgroundColor = randomColor();
+    // enemyCar.style.backgroundImage = "url(images/car.png)";
     // enemyCar.style.background=enemyCarImage();
     enemyCar.style.left = Math.floor(Math.random() * 350) + "px";
     road.appendChild(enemyCar);
